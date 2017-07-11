35864ff6ca5606b5bcdd74bf30cb9eacb49e53f7
Diego Camacho <hackvan@gmail.com>
2017-07-11 14:02:48 -0500

    Adicionar la funcionalidad para la creación de productos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/products', to: 'products#index'
-  get '/products/new', to: 'products#new'
-  
+  get  '/products',     to: 'products#index'
+  get  '/products/new', to: 'products#new', as: 'new_product'
+  post 'products',      to: 'products#create'
 end
