dc6cfff233d07d43f6dfd4ebaacd7ad7d38570f1
Diego Camacho <hackvan@gmail.com>
2017-07-13 13:59:03 -0500

    Crea la ruta PATCH /products/:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get  '/products',          to: 'products#index'
-  get  '/products/new',      to: 'products#new', as: 'new_product'
-  post 'products',           to: 'products#create'
-  get  '/products/:id',      to: 'products#show', as: 'product'
-  get  'products/:id/edit',  to: 'products#edit'
+  get   '/products',          to: 'products#index'
+  get   '/products/new',      to: 'products#new', as: 'new_product'
+  post  'products',           to: 'products#create'
+  get   '/products/:id',      to: 'products#show', as: 'product'
+  get   'products/:id/edit',  to: 'products#edit'
+  patch 'products/:id',       to: 'products#update'
 end
