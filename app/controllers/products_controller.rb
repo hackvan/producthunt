24828ed13f3e4d71b94cef45d7689de9861ec6b8
Diego Camacho <hackvan@gmail.com>
2017-07-14 13:16:00 -0500

    Crea la ruta DELETE /products/:id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, notice: "El producto fue eliminado con éxito"
+  end
+
   private
     def product_params
       params.require(:product).permit(:name, :url, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get   '/products',          to: 'products#index'
-  get   '/products/new',      to: 'products#new', as: 'new_product'
-  post  'products',           to: 'products#create'
-  get   '/products/:id',      to: 'products#show', as: 'product'
-  get   '/products/:id/edit', to: 'products#edit', as: 'edit_product'
-  patch 'products/:id',       to: 'products#update'
+  get    '/products',          to: 'products#index'
+  get    '/products/new',      to: 'products#new', as: 'new_product'
+  post   'products',           to: 'products#create'
+  get    '/products/:id',      to: 'products#show', as: 'product'
+  get    '/products/:id/edit', to: 'products#edit', as: 'edit_product'
+  patch  'products/:id',       to: 'products#update'
+  delete 'products/:id',       to: 'products#destroy'
 end
