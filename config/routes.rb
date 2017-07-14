3d4382a35ec4a64ae4830a1c8aa089ed88921569
Diego Camacho <hackvan@gmail.com>
2017-07-14 13:29:19 -0500

    Agrega la ruta root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  root 'products#index'
   # get    '/products',          to: 'products#index'
   # get    '/products/new',      to: 'products#new', as: 'new_product'
   # post   'products',           to: 'products#create'
