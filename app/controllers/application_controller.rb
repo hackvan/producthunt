e9a5903ffe26c7c00e5e9230114f6f3ae4514773
Diego Camacho <hackvan@gmail.com>
2017-07-18 13:56:07 -0500

    Restringe las páginas privadas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,14 @@ class ApplicationController < ActionController::Base
     @current_user = nil
   end
 
+  def private_access
+    redirect_to :login unless signed_in?
+  end
+
+  def public_access
+    redirect_to root_path if signed_in?
+  end
+  
   private
     def signed_in?
       !current_user.nil?
@@ -22,5 +30,5 @@ class ApplicationController < ActionController::Base
     rescue ActiveRecord::RecordNotFound
     end
     helper_method :current_user
-    
+
 end
