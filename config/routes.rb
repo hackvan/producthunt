ec8f103a02781c64cd8b197be76891a3c6a5158f
Diego Camacho <hackvan@gmail.com>
2017-07-18 13:45:45 -0500

    Implementa la funcionalidad de salir

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'products#index'
 
-  get  'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
+  get    'login', to: 'sessions#new'
+  post   'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create]
   resources :products
