3e0fdf02c5289eabcabce90200aa42a41e8a1a45
Diego Camacho <hackvan@gmail.com>
2017-07-24 14:09:44 -0500

    Implementa la funcionalidad de comentarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@
 
 class User < ApplicationRecord
   has_secure_password validations:false
+
+  has_many  :products
+  has_many  :comments
+
   validates :email, uniqueness: true, format: /@/
   validates :password, presence: true, on: :create
   validates :password, length: { in: 6..20 }, allow_nil: true
